58a724fc7c0910292727701db4181aef42f9c9bd
joseph <joearockiam@gmail.com>
2015-06-20 22:06:05 +0800

    commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 ##Analysis
-##Step 1(Merge the training and the test sets to create one data set)
+##1 - Merge the training and the test sets to create one data set.
 # read all the test data set.
 test_labels <- read.table("test/y_test.txt", col.names="label")
 test_subjects <- read.table("test/subject_test.txt", col.names="subject")
@@ -16,7 +16,7 @@ train_data_subject_label<-cbind(train_subjects, train_labels, train_data)
 #merge training and test data
 data <- rbind(test_data_subject_label, train_data_subject_label)
 
-##Step 2(Extracts only the measurements on the mean and standard deviation for each measurement)
+##2 - Extracts only the measurements on the mean and standard deviation for each measurement.
 # read feature data.
 features <- read.table("features.txt", stringsAsFactors=FALSE)
 
@@ -27,14 +27,14 @@ features_mean_std <- features[grep("mean\\(\\)|std\\(\\)", features$V2), ]
 #data starts from 3rd column onwards.
 data_mean_std <- data[, c(1, 2, features_mean_std$V1+2)]
 
-## step 3(Descriptive activity names to name the activities in the data set)
+##3 - Descriptive activity names to name the activities in the data set.
 # read the activity labels
 activity_labels <- read.table("activity_labels.txt")
 
 # replace labels in data with label description
 data_mean_std$label <- activity_labels[data_mean_std$label, 2]
 
-## step 4(Appropriately labels the data set with descriptive variable names)
+##4 - Appropriately labels the data set with descriptive variable names.
 # first make a list of the current column names and feature names
 col_names <- c("subject", "activitylabel", features_mean_std$V2)
 
@@ -44,7 +44,7 @@ col_names<-tolower( gsub("[^[:alnum:] ]", "", col_names))
 # then use the list as column names for data
 colnames(data_mean_std) <- col_names
 
-## step 5(creates a second, independent tidy data set with the average of each variable for each activity and each subject)
+##5 - creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 # find the tidy data set with the average of each variable for each activity and each subject.
 aggregateData <- aggregate(data_mean_std[, 3:ncol(data_mean_std)],by=list(subject = data_mean_std$subject,activitylabel = data_mean_std$activitylabel), mean)
 
